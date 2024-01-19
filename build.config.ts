637f5e8a86447041cf9ed2657924e52f326f298c
cole <bernankeic@gmail.com>
2024-01-19 09:59:47 +0800

    chore: bin file

--- build.config.ts
+++ build.config.ts
@@ -3,13 +3,11 @@ import { defineBuildConfig } from "unbuild";
 export default defineBuildConfig({
   entries: [
     "src/index",
+    "src/cli",
   ],
   // TODO export only one declaration file
   // export cli file
   // set bin
   declaration: true,
   clean: true,
-  rollup: {
-    emitCJS: true,
-  },
 });
